c538be7328c431e63de6d99799393b86192d88e8
reet-571099 <63258652+reet-571099@users.noreply.github.com>
2021-06-22 20:13:35 +0530

    Upload Done

--- BtechP/main.py
+++ BtechP/main.py
@@ -28,7 +28,7 @@
 #     app.run(debug=True)
 
 
-from flask import Blueprint, render_template,request
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 import os
 
@@ -57,13 +57,36 @@ def about():
     return render_template('about.html')
 
 
-@main.route('/success', methods = ['GET','POST'])
-def success():
-    if request.method == 'POST':
+# @main.route('/success', methods = ['GET','POST'])
+# def success():
+#     if request.method == 'POST':
+#         f = request.files['file']
+#         location = os.path.join(upload_folder,f.filename)
+#         f.save(location)
+#         return render_template("dashboard.html", name=f.filename)
+
+@main.route('/dashboard', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    else:
         f = request.files['file']
-        location = os.path.join(upload_folder,f.filename)
+        location = os.path.join(upload_folder, f.filename)
         f.save(location)
-        return render_template("dashboard.html", name=f.filename)
+        flash('Image successfully uploaded ')
+        return render_template("dashboard.html", filename=f.filename)
 
 
-
+# @main.route("/dashboard", methods=['POST'])
+# def button():
+#        v = 0
+#        if request.method == "POST":
+#         if upload_image.filename == "4207.jpeg":
+#             return render_template("dashboard.html", result=0)
+#         else :
+#             return render_template("dashboard.html", result=1)
